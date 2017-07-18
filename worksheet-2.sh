03f3abfaad7f9dbd33648cebaa764949923be6ee
vaprescott <vprescott@luc.edu>
2017-07-18 17:11:05 +0000

    updating git config

--- worksheet-2.sh
+++ worksheet-2.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 ## Configure git
-git config --global user.name ...
-git config --global user.email ...
+git config --global user.name vaprescott
+git config --global user.email vprescott@luc.edu
 
 ## Change the "origin" remote URL and push
-git remote set-url origin ...
+git remote set-url origin git@github.com:vaprescott/handouts.git
+git push -u origin master
 
 ## Set the SESYNC-CI repository upstream and pull updates
 git remote add upstream https://github.com/sesync-ci/handouts.git
